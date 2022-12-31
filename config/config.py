3690e4ac569f3b3365be4c9730a3102c66ad9408
SERYFJOP <118700373+SERYFJOP@users.noreply.github.com>
2022-12-31 22:20:02 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -147,7 +147,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(
 
 
 # You'll need a Pyrogram String Session for these vars. Generate String from our session generator bot @YukkiStringBot
-STRING1 = getenv("STRING_SESSION", Nonr)
+STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
